1f09c4e5f2bfa0ae162e4f5fe6cecf3f208c833d
GrandF17 <v.o.nikolenko@gmail.com>
2023-04-07 13:36:44 +0300

    finished EC.cpp + .gitignore was addes

--- EllipticCurve/EC.cpp
+++ EllipticCurve/EC.cpp
@@ -1,69 +1,117 @@
 #include <openssl/obj_mac.h>
 #include <openssl/ec.h>
+#include <thread>
+#include <vector>
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-// void *addPoint(void *arg)
-// {
-//     EC_POINT_add(group, new_point, new_point, points[0], ctx);
-
-//     pthread_exit(NULL);
-// }
-
+// COMPILATION:
 // gcc -g -Wall -Wextra -o EC EC.cpp -lcrypto -lstdc++
+
+// -_-_-_ behaviaral constants
+const bool USE_THREADS = false;
+const bool SHOW_POINTS = false;
+const bool SHOW_COUNTER = true;
+
+// -_-_-_ global vars -_-_-_
+EC_GROUP *group;
+BN_CTX *ctx;
+EC_POINT *point;
+EC_POINT **points;
+EC_POINT *new_point;
+
+long long iteration_count = 0;
+
+void *addPoint(void *arg)
+{
+    // Дублируем текущую точку
+    new_point = EC_POINT_dup(point, group);
+
+    // Складываем текущую точку с начальной точкой
+    EC_POINT_add(group, new_point, new_point, points[0], ctx);
+
+    // Добавляем новую точку в массив
+    iteration_count++;
+    points[iteration_count] = new_point;
+
+    // Переходим к следующей итерации
+    point = new_point;
+}
 
 int main(void)
 {
-    EC_GROUP *group = EC_GROUP_new_by_curve_name(NID_secp112r1);
-    EC_POINT *point = EC_POINT_new(group);
+    group = EC_GROUP_new_by_curve_name(NID_secp112r1);
+    point = EC_POINT_new(group);
 
-    BN_CTX *ctx = BN_CTX_new();
+    ctx = BN_CTX_new();
     BIGNUM *x = BN_new();
     BIGNUM *y = BN_new();
     BN_hex2bn(&x, "09487239995A5EE76B55F9C2F098");
     BN_hex2bn(&y, "A89CE5AF8724C0A23E0E0FF77500");
     EC_POINT_set_affine_coordinates_GFp(group, point, x, y, ctx);
 
-    cout << "kek" << endl;
     long long max_iterations = (long long)pow(2, 32); // cant use 57 bits :(
-    cout << "kek" << endl;
-    long long iteration_count = 0;
 
-    cout << "kek" << endl;
-    EC_POINT **points = new EC_POINT *[max_iterations];
-    cout << "kek" << endl;
+    points = new EC_POINT *[max_iterations];
     points[0] = EC_POINT_dup(point, group);
-    cout << "kek" << endl;
 
-    cout << "kek" << endl;
+    long long compNum = (long long)pow(2, 31);
+
     while (iteration_count < max_iterations)
     {
-        // Дублируем текущую точку
-        EC_POINT *new_point = EC_POINT_dup(point, group);
-
-        cout << new_point << endl;
-
-        // Складываем текущую точку с начальной точкой
-        EC_POINT_add(group, new_point, new_point, points[0], ctx);
-
-        // Сравниваем новую точку с предыдущими точками
-        for (int i = 1; i <= iteration_count; i++)
+        if (iteration_count <= compNum && USE_THREADS)
         {
-            if (EC_POINT_cmp(group, new_point, points[i], ctx) == 0)
+            int limit = (int)pow(2, 8); // eq 4096 threads per operation
+
+            pthread_t threads[limit];
+            for (int i = 0; i < limit; i++)
             {
-                // Найдена периодическая последовательность
-                std::cout << "Period found: " << i << std::endl;
-                return 0;
+                int *p;
+                int rc = pthread_create(&threads[i], NULL, addPoint, (void *)(p));
+
+                if (rc)
+                {
+                    cout << "Error:unable to create thread," << rc << endl;
+                    exit(-1);
+                }
             }
+            for (int i = 0; i < limit; i++)
+                pthread_join(threads[i], NULL);
+
+            cout << iteration_count << endl;
         }
+        else
+        {
+            // Дублируем текущую точку
+            new_point = EC_POINT_dup(point, group);
 
-        // Добавляем новую точку в массив
-        iteration_count++;
-        points[iteration_count] = new_point;
+            // Складываем текущую точку с начальной точкой
+            EC_POINT_add(group, new_point, new_point, points[0], ctx);
 
-        // Переходим к следующей итерации
-        point = new_point;
+            // Сравниваем новую точку с предыдущими точками
+            for (int i = 1; i <= iteration_count; i++)
+            {
+                if (EC_POINT_cmp(group, new_point, points[i], ctx) == 0)
+                {
+                    // Найдена периодическая последовательность
+                    cout << "Period found: " << i << endl;
+                    return 0;
+                }
+            }
+
+            if (SHOW_POINTS)
+                cout << new_point << endl;
+            if (SHOW_COUNTER && iteration_count % 1024 == 0)
+                cout << iteration_count << endl;
+
+            // Добавляем новую точку в массив
+            iteration_count++;
+            points[iteration_count] = new_point;
+
+            // Переходим к следующей итерации
+            point = new_point;
+        }
     }
 
     EC_POINT_free(point);
