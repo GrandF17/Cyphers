b6089a016b843bf88dead2a3782b2723cbd4376d
GrandF17 <v.o.nikolenko@gmail.com>
2024-04-25 16:47:16 +0300

    3/4 of work done

--- KuzNech&C/interface/interfaces.h
+++ KuzNech&C/interface/interfaces.h
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include <cstdint>
 #include <iostream>
 #include <vector>
@@ -15,5 +17,46 @@ using namespace std;
 typedef struct {
     vector<vector<uint8_t>> keys[KUZ_CONST::ROUNDS_AMOUNT];
 } roundKeys;
+
+/**
+ * @param str is regex value for which we search in argv
+ * @param param is current parameter extracted out of braces (casually path to a file)
+ */
+template <typename T>
+class ArgParam {
+   public:
+    regex regPattern;
+    T param;
+
+    ArgParam(const regex& _pattern)
+        : regPattern(_pattern) {}
+};
+
+/**
+ * @param logFile   - where logs will be put
+ * @param file      - data to ecnrypt/decript
+ * @param key       - file with bit sequence length >= 256 bits
+ * @param offset    - offset in file that contains key (only if there are more than 256) default = 0
+ */
+class ProgramParams {
+   public:
+    ArgParam<string>* logFile;
+    ArgParam<string>* file;
+    ArgParam<string>* key;
+    ArgParam<size_t>* offset;
+
+    ProgramParams()
+        : logFile(new ArgParam<string>(regex("--l=([^\"]*)"))),
+          file(new ArgParam<string>(regex("--f=([^\"]*)"))),
+          key(new ArgParam<string>(regex("--k=([^\"]*\\.key)"))),
+          offset(new ArgParam<size_t>(regex("--o=([^\"]*)"))) {}
+
+    ~ProgramParams() {
+        delete logFile;
+        delete file;
+        delete key;
+        delete offset;
+    }
+};
 
 #endif
--- KuzNech&C/ofb.cpp
+++ KuzNech&C/ofb.cpp
@@ -1,10 +1,12 @@
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <regex>
 #include <vector>
 
 #include "./classes/keyGen.cpp"
 #include "./classes/logger.cpp"
+#include "./interface/interfaces.h"
 #include "kuznechik.cpp"
 
 using namespace std;
@@ -12,11 +14,58 @@ using namespace std;
 static vector<uint8_t> block =
     {0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11};
 
-int main() {
+size_t paramIndex(int argc, char** argv, regex pattern) {
+    for (size_t i = 0; i < argc; i++) {
+        string str = argv[i];
+        if (regex_match(str, pattern)) return i;
+    }
+
+    throw invalid_argument("Not found parameter, make sure you are entering correct values!");
+}
+
+string param(int argc, char** argv, regex pattern) {
+    string input = argv[paramIndex(argc, argv, pattern)];
+    regex argPattern("=([^\\s]*)");
+    smatch match;
+
+    if (regex_search(input, match, argPattern)) {
+        if (match.size() > 1) {
+            string extractedString = match[1];
+            return extractedString;
+        }
+    }
+
+    throw invalid_argument("Incorrect value in argv string. Make sure you are entering correct values!");
+}
+
+/**
+ * recursive function to search all
+ * parameters passed in argv[] array
+ */
+ProgramParams extractProgramParams(int argc, char** argv) {
+    ProgramParams params;
+    regex pattern("--[a-zA-Z]+=\\\"([^\"]*)\\\"");
+
+    params.file->param = param(argc, argv, params.file->regPattern);
+    params.logFile->param = param(argc, argv, params.logFile->regPattern);
+    params.key->param = param(argc, argv, params.key->regPattern);
+    params.offset->param = (size_t)stoull(param(argc, argv, params.offset->regPattern));
+
+    return params;
+}
+
+int main(int argc, char** argv) {
+    /**
+     * main class that copntains all arguments
+     */
+    ProgramParams params = extractProgramParams(argc, argv);
     KeyGen keyGen;
+    Logger logger = Logger(params.logFile->param, false);
+
+    /**
+     * to store logs in main and then write them down to file
+     */
     vector<string> logs;
-    Logger logger = Logger("logs.txt", false);
-    string fileName = "./1048576.key";
 
     /**
      * writing logs to vector (1 step)
@@ -26,7 +75,7 @@ int main() {
         logs.push_back(logger.bytesToString(block));
     }
 
-    vector<vector<uint8_t>> keys = keyGen.generateRoundKeys(fileName, 100);
+    vector<vector<uint8_t>> keys = keyGen.generateRoundKeys(params.key->param, params.offset->param);
 
     /**
      * writing logs to vector (2 step)
